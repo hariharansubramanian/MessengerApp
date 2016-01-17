73dddfdd932c7fcf2bdd006415e2630c8779b857
Hariharan Subramaniam <hari.s@plank.life>
2016-01-17 19:46:13 +0530

    Added parse initialziation to mainActivity Added metadata of client and public keys to manifest file

--- app/src/main/java/com/example/plank_haris/messengerapp/MainActivity.java
+++ app/src/main/java/com/example/plank_haris/messengerapp/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.parse.Parse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +32,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+//        if(savedInstanceState==null){
+//            getFragmentManager().beginTransaction()
+//                    .add(R.id.container,new MainActivityFragment())
+//                    .commit();
+//        }
+
     }
 
     @Override
